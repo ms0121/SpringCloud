6adc09b520996d06b28dd694bbf52732b0789cef
ms0121 <1132601565@qq.com>
2021-06-24 21:49:25 +0800

    学习记录

---
+++ cloud2021/springcloud-config-center3344/src/main/java/com/liu/cloud/ConfigCloudMain3344.java
@@ -0,0 +1,15 @@
+package com.liu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lms
+ * @date 2021-06-24 - 21:48
+ */
+@SpringBootApplication
+public class ConfigCloudMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCloudMain3344.class, args);
+    }
+}
