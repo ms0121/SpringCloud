bd93ab2d823b1703c5f966862f242ba56630be08
ms0121 <1132601565@qq.com>
2021-06-24 22:15:45 +0800

    学习记录

--- cloud2021/alibaba-nacos-provider9001/src/main/java/com/liu/cloud/NacosCloudMain9001.java
+++ cloud2021/alibaba-nacos-provider9001/src/main/java/com/liu/cloud/NacosCloudMain9001.java
@@ -2,16 +2,17 @@ package com.liu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author lms
- * @date 2021-06-24 - 21:48
+ * @date 2021-06-24 - 22:12
  */
+
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigCloudMain3344 {
+@EnableDiscoveryClient
+public class NacosCloudMain9001 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCloudMain3344.class, args);
+        SpringApplication.run(NacosCloudMain9001.class, args);
     }
 }
