21a9f279e8b042b3299f9fe0503dccd7cf8d1bb5
ms0121 <1132601565@qq.com>
2021-06-24 11:50:30 +0800

    学习记录

---
+++ cloud2021/springcloud-stream-rabbitmq-provider8801/src/main/java/com/liu/cloud/StreamCloudMain8801.java
@@ -0,0 +1,16 @@
+package com.liu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lms
+ * @date 2021-06-24 - 11:49
+ */
+
+@SpringBootApplication
+public class StreamCloudMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamCloudMain8801.class, args);
+    }
+}
