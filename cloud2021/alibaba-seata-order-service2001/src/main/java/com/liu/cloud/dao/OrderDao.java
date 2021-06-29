5d0f5bfc64262283f557ed60e27868c5fff04b99
ms0121 <1132601565@qq.com>
2021-06-29 23:51:16 +0800

    学习记录

--- cloud2021/alibaba-seata-order-service2001/src/main/java/com/liu/cloud/dao/OrderDao.java
+++ cloud2021/alibaba-seata-order-service2001/src/main/java/com/liu/cloud/dao/OrderDao.java
@@ -1,10 +1,18 @@
 package com.liu.cloud.dao;
 
+import com.liu.cloud.domain.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * @author lms
  * @date 2021-06-29 - 21:33
  */
 
+@Mapper  // 扫描当前的dao包
 public interface OrderDao {
 
+    void create(Order order);
+
+    void update(@Param("userId") String userId, @Param("status") Integer status);
 }
